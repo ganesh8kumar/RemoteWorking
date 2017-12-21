fba5e31787003bc581d00350ab3a042799e8f9db
ganesh8kumar <34685396+ganesh8kumar@users.noreply.github.com>
2017-12-21 13:49:20 -0500

    Add P2 instance to main

--- main.cpp
+++ main.cpp
@@ -6,18 +6,15 @@ using std::endl;
 
 int main()
 {
-    cout << "My Friends:\n\n";
+    cout << "People:\n\n";
 
     Person p1("Jonlou", "Biron");
     cout << "\t" << p1.ToString() << endl;
 
-	Person p2("Rick", "Cohen");
+    Person p2("Rick", "Cohen");
     cout << "\t" << p1.ToString() << endl;
 	
-	Person p3("Wn", "Cn");
-    cout << "\t" << p1.ToString() << endl;
-    
-	cout << endl;
+    cout << endl;
 
     return 0;
 }
